b02fe6ce447a4d532cac630d5dbc50545676735b
ngonyo <77175971+ngonyo@users.noreply.github.com>
2023-08-07 19:14:06 -0500

    Add files via upload

---
+++ 0_Python Basics.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 12 08:37:46 2023
+
+@author: nathan
+"""
+
+#this is a comment
+
+print("die thomas die")
+print(13)
+z = "die thomas"
+y = 'die thomas'
+x = 13
+type(x)
+type(y)
+type(z)
+print(type(x))
+
+#python is a soft typed language. c++ is strongly typed
+
+n = 0.0
+type(n)
+
+#n+=4
+#this is the same as n = n + 4
+n = 'TACO'
+print(n)
+
+#Truncation / Rounding
+minute = 59
+print(minute/60)
+#notice that standard divion does not trunkcate like in c++
+
+#Standard math works
+x = 10
+y = 15
+print(x+y)
+
+print(3**2)
+
+#concatenate strings (or append string to another)
+s = 'taco'
+t = 'burrito'
+print()
+
+#print multiple things at once
+print('abba', 'ooga')
+print(n*10)
+
+#input variable
+gimmeyourorder = input()
+print(gimmeyourorder)
+gimmeyourorder = input("whatchu want bro?")
+print(gimmeyourorder)
+
+
+
+
